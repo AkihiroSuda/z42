72c6fc29c5eaeef17c02bab821584d82f8c3d609
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2015-12-31 03:16:49 +0900

    fix(setup): fix packages

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
     name='z42',
-    packages=['z42'],
-    version='0.0.1',
+    packages=['z42', 'z42.drivers'],
+    version='0.0.2',
     description='Zeroconf (mDNS) for Amazon EC2',
     author='Akihiro Suda',
     author_email='suda.kyoto@gmail.com',
     url='https://github.com/AkihiroSuda/z42',
-    download_url='https://github.com/AkihiroSuda/z42/tarball/v0.0.1',
+    download_url='https://github.com/AkihiroSuda/z42/tarball/v0.0.2',
     license='Apache License 2.0',
     scripts=['bin/z42'],
     install_requires=[
